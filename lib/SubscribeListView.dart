ad95c8dee0e7aa0cba8c4c2e3f95ab8c5d45dc99
Annitas <39757257+Annitas@users.noreply.github.com>
2023-12-05 13:41:09 +0600

    Add intrests text

---
+++ lib/SubscribeListView.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class SubscribeListView extends StatelessWidget {
+  const SubscribeListView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            'Subscribed Users',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ),
+        ListView(
+          padding: const EdgeInsets.all(8),
+          scrollDirection: Axis.horizontal,
+          children: [
+            Padding(
+              child: Text("Tom", style: TextStyle(fontSize: 22)),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+            ),
+            Padding(
+              child: Text("Alice", style: TextStyle(fontSize: 22)),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+            ),
+            Padding(
+              child: Text("Bob", style: TextStyle(fontSize: 22)),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+            ),
+            Padding(
+              child: Text("Sam", style: TextStyle(fontSize: 22)),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+            ),
+            Padding(
+              child: Text("Kate", style: TextStyle(fontSize: 22)),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'UserView.dart';
 import 'SubscribeView.dart';
+import 'SubscribeListView.dart';
 
 void main() {
   runApp(
@@ -20,11 +21,13 @@ class MyApp extends StatelessWidget {
           children: <Widget>[
             UserView(),
             // TabBarApp(),
+
             SubscribeView(),
 
+            // SubscribeListView(),
+            TariffsAndLimitsView(),
 
-
-         
+            Intrests(),
           ],
         ),
       )
@@ -32,25 +35,66 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class SubscribeListView extends StatelessWidget {
-  const SubscribeListView({super.key});
+
+
+class TariffsAndLimitsView extends StatelessWidget {
+  const TariffsAndLimitsView({super.key});
   @override
   Widget build(BuildContext context) {
-    return ListView(
-                children: const <Widget>[
-                  DecoratedBox(
-                    decoration: BoxDecoration(color: Colors.blue),
-                    child: ListTile(
-                      title: Text('Item 1'),
-                    ),
-                  ),
-                  DecoratedBox(
-                    decoration: BoxDecoration(color: Colors.green),
-                    child: ListTile(
-                      title: Text('Item 2'),
-                    ),
-                  ),
-                ],
+    return Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start, // выравнивание текста слева
+            children: const <Widget>[
+              Text(
+                  'Тарифы и лимиты                     ',
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(   // зеленый цвет текста
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700
+                  )
+              ),
+              Text(
+                  'Для операций в Сбербанк Онлайн                ',
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey,
+                  )
+              ),
+            ]
+        )
+    );
+  }
+}
+
+class Intrests extends StatelessWidget {
+  const Intrests({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start, // выравнивание текста слева
+            children: const <Widget>[
+              Text(
+                  'Интересы',
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(   // зеленый цвет текста
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700
+                  )
+              ),
+              Text(
+                  'Мы подбираем истории и предложения по темам, которые вам нравятся',
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey,
+                  )
+              ),
+            ]
+        )
     );
   }
 }
